3a03893cc5ab8489e5c1e516fc73cfa1ae5e2d08
Mark Jaroski <mark@geekhive.net>
2010-01-28 21:16:24 +0100

    Missed a couple of files in the previous commit.

---
+++ lib/Bric/Util/Grp/Job/Dist.pm
@@ -0,0 +1,5 @@
+package Bric::Util::Grp::Job::Dist;
+use constant OBJECT_CLASS_ID => 79;
+use base qw(Bric::Util::Grp::Job);
+1;
+
---
+++ lib/Bric/Util/Grp/Job/Pub.pm
@@ -0,0 +1,4 @@
+package Bric::Util::Grp::Job::Pub;
+use constant OBJECT_CLASS_ID => 80;
+use base qw(Bric::Util::Grp::Job);
+1;
